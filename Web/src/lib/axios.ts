03825d616e2a2fc6b1f58fc1da1fbde2a4bd5d9d
Dionatan <dionatan.1751@gmail.com>
2023-01-27 11:09:47 -0300

    digitalocean

--- Web/src/lib/axios.ts
+++ Web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://53b0-2804-14c-c683-8141-75ec-976c-7212-7f8e.sa.ngrok.io'
 })
